3a43e568f2996466f8fb8a7aa295ee6ded7c28e0
Zi9 <kevin.lumik@gmail.com>
2020-06-15 14:56:52 +0300

    Better build script

--- build.fish
+++ build.fish
@@ -3,7 +3,8 @@ echo "Building Blender addon..."
 mkdir zi_cx_tools
 cp ./MapTools.py zi_cx_tools
 cp ./__init__.py zi_cx_tools
-zip carx_map_tools.zip ./zi_cx_tools/*
+set VER (grep version __init__.py | cut -d '(' -f 2 | cut -d ')' -f 1 | sed 's/, /./g')
+zip carx_map_tools_v$VER.zip ./zi_cx_tools/*
 echo "Build finished. Cleaning..."
 rm -r zi_cx_tools
 echo "Done"
